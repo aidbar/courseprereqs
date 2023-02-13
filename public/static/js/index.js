e189946214a5774fcb730a53c55521ea304bf8a3
aidbar <47143229+aidbar@users.noreply.github.com>
2023-02-13 13:54:09 +0200

    Dropdown with "onchange" event listener

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -4,8 +4,73 @@ import axios from 'https://cdn.skypack.dev/axios';
 const dataSet = async function getData() {
     return await axios.get('/data');
 }
-async function drawChart() {
+
+var courseData = [];
+var selectedCourse = "";
+
+function hasJsonStructure(str) {
+    if (typeof str === 'string') return false;
+    try {
+        return Object.prototype.toString.call(str) === '[object Object]' || Array.isArray(str);
+    } catch (err) {
+        return false;
+    }
+};
+
+
+//Parse and convert object recursive
+function walk2(obj2,level) {
+    var mytempCH = [];
+    var outer = [];
+    for (var key in obj2){
+        var value = obj2[key];
+        if(hasJsonStructure(value)){
+            //has children
+            var mytempCH2 = new Object();
+            mytempCH2.name = key;
+            mytempCH2.children = walk2(value,level+1);
+            outer.push(mytempCH2);
+        }else{
+            outer.push({"name": key + ": " + value, "value": level})
+        }
+      }
+      return outer
+}
+
+function getNodes() {
+    console.log("getNodes() is running.");
+}
+
+function onChange() {
+    console.log("*********************onChange event listener");
+}
+
+async function drawGraph() {
     const data = await dataSet();
+    console.log(data);
+
+    /*var myNewObj = new Object();
+    myNewObj.name = "ConvertedObj";
+    myNewObj.children = [];
+    var level = 1;
+    //Start work
+    myNewObj.children = walk2(data,1);
+
+    console.log(myNewObj);*/
+
+    courseData = data.data;
+    console.log(courseData);
+
+    let dropdownSelection = d3.select("select");
+    let dropdownArray = dropdownSelection._groups[0];
+    let dropdown = dropdownArray[0];
+    console.log("dropdown is:");
+    console.log(dropdown);
+    dropdown.addEventListener("change",onChange);
+
+    //getNodes(courseData);
+    
+    /*const data = await dataSet();
     const svgWidth = 500;
     const svgHeight = 500;
     const barPadding = 5;
@@ -28,6 +93,6 @@ async function drawChart() {
             let translate = [barWidth * i, 0];
             return `translate(${translate})`;
         })
-        .style("fill", "steelblue");
+        .style("fill", "steelblue");*/
 }
-drawChart();
+drawGraph();
--- server.js
+++ server.js
@@ -2,11 +2,32 @@ const express = require('express');
 const { engine } = require('express-handlebars');
 const app = express();
 const fs = require('fs');
+const handlebars = require('express-handlebars');
 const port = 3000;
 app.use(express.static('public'));
-app.engine('handlebars', engine());
+var handlebarsHelpers = handlebars.create({
+    helpers: {
+        /*sayHello: function () { alert("Hello World") },
+        getStringifiedJson: function (value) {
+            return JSON.stringify(value);
+        }*/
+        dropdownChange:  function (value) {
+            console.log("helper running.");
+            console.log("value is " + value);
+            return value;
+        }
+    }
+    /*defaultLayout: 'main',
+    partialsDir: ['views/partials/']*/
+});
+app.engine('handlebars', handlebarsHelpers.engine); //app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
+
+/*engine().registerHelper('dropdownChange', function (value) {
+    console.log("helper running.");
+    console.log("value is " + value);
+});*/
 
 app.get('/', (req, res) => {
     console.log("app.get for /");
@@ -25,11 +46,10 @@ app.get('/data', (req, res) => {
     fs.readFile('./data/data.json', (err, courseDataResponse) => {
         if (err) throw err;
         const courseDataUnfiltered = JSON.parse(courseDataResponse);
-        console.log(courseDataUnfiltered);
+        //console.log(courseDataUnfiltered);
         res.json(courseDataUnfiltered);
     });  
 });
-
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
