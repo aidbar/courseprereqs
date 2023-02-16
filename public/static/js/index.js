7524f8aed0b099064f896dfb940608475d83969f
aidbar <47143229+aidbar@users.noreply.github.com>
2023-02-16 13:49:08 +0200

    Graph is being drawn!!!!! <3

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -18,6 +18,9 @@ var selectedCourseIndex = -1;
 var nodeIndex = 1;
 var parentNodeIndex = 1;
 
+var link;
+var node;
+
 function hasJsonStructure(str) {
     if (typeof str === 'string') return false;
     try {
@@ -221,7 +224,72 @@ function drawGraph() {
     console.log("drawGraph running");
     graphData = {"nodes" : graphNodeData, "links": graphLinkData};
     console.log(graphData);
+
+    // set the dimensions and margins of the graph
+    var margin = {top: 10, right: 30, bottom: 30, left: 40},
+    width = 400 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3.select("svg")
+    //.append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+    console.log("**************************after var svg");
+
+    //d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_network.json", function( data) {
+
+    // Initialize the links
+    link = svg
+    .selectAll("line")
+    .data(graphData.links)
+    .enter()
+    .append("line")
+        .style("stroke", "#aaa")
+
+    // Initialize the nodes
+    node = svg
+    .selectAll("circle")
+    .data(graphData.nodes)
+    .enter()
+    .append("circle")
+        .attr("r", 20)
+        .style("fill", "#69b3a2")
+    
+    console.log("**************link, node");
+
+    // Let's list the force we wanna apply on the network
+    var simulation = d3.forceSimulation(graphData.nodes)                 // Force algorithm is applied to data.nodes
+        .force("link", d3.forceLink()                               // This force provides links between nodes
+            .id(function(d) { return d.id; })                     // This provide  the id of a node
+            .links(graphData.links)                                    // and this the list of links
+        )
+        .force("charge", d3.forceManyBody().strength(-400))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
+        .force("center", d3.forceCenter(width / 2, height / 2))     // This force attracts nodes to the center of the svg area
+        .on("end", ticked);
+
+    //});
+
+    console.log("**********simulation");
+
 }
+
+function ticked() {
+    link
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node
+        .attr("cx", function (d) { return d.x+6; })
+        .attr("cy", function(d) { return d.y-6; });
+}
+    
 
 async function initialize() {
     const data = await dataSet();
